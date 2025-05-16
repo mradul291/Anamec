593748ff3cf5fa31ac97b2b79708c167da355659
mradul291 <mishramradul29@gmail.com>
2025-05-16 11:55:18 +0000

    delivery challand and calibration doctype added

---
+++ anamec_instruments/anamec_instruments/doctype/calibration/calibration.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Techsolvo LLP and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Calibration(Document):
+	pass
---
+++ anamec_instruments/anamec_instruments/doctype/calibration/test_calibration.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Techsolvo LLP and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestCalibration(FrappeTestCase):
+	pass
