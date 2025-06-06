2c065f0503295854c808327a72a19eb9bd3e7ad8
mradul291 <mishramradul29@gmail.com>
2025-06-06 19:04:13 +0530

    Color on date in calibration

--- anamec_instruments/anamec_instruments/doctype/calibration/calibration_list.js
+++ anamec_instruments/anamec_instruments/doctype/calibration/calibration_list.js
@@ -1,21 +1,27 @@
-// frappe.listview_settings['Calibration'] = {
-//     get_indicator: function (doc) {
-//         if (!doc.next_due_date) return;
+frappe.listview_settings['Calibration'] = {
+    formatters: {
+        "next_due_date": function (value, df, doc) {
+            if (!doc.next_due_date) return "";
 
-//         const due = moment(doc.next_due_date, "DD-MM-YYYY");  // parse from custom format
-//         const today = moment(); // today's date in moment
+            const due = moment(doc.next_due_date, "YYYY-MM-DD");
+            const today = moment();
 
+            console.log("next_due_date:", doc.next_due_date);
 
+            let color_class = "red"; // default
 
-//         if (due.isSame(today, 'day')) {
-//             console.log(` 1 Due: ${due.format("DD-MM-YYYY")} | Today: ${today.format("DD-MM-YYYY")}`);
-//             return [doc.next_due_date, 'red'];
-//         } else if (due.isAfter(today, 'day')) {
-//             console.log(` 2 Due: ${due.format("DD-MM-YYYY")} | Today: ${today.format("DD-MM-YYYY")}`);
-//             return [doc.next_due_date, 'green',];
-//         } else {
-//             console.log(` 3 Due: ${due.format("DD-MM-YYYY")} | Today: ${today.format("DD-MM-YYYY")}`);
-//             return [doc.next_due_date, 'darkgrey',];
-//         }
-//     }
-// };
+            if (due.isSame(today, 'day')) {
+                console.log(`1 Due: ${due.format("DD-MM-YYYY")} | Today: ${today.format("DD-MM-YYYY")}`);
+                color_class = "red";
+            } else if (due.isAfter(today, 'day')) {
+                console.log(`2 Due: ${due.format("DD-MM-YYYY")} | Today: ${today.format("DD-MM-YYYY")}`);
+                color_class = "green";
+            } else {
+                console.log(`3 Due: ${due.format("DD-MM-YYYY")} | Today: ${today.format("DD-MM-YYYY")}`);
+                color_class = "dark";
+            }
+
+            return `<span class="indicator-pill ${color_class} filterable no-indicator-dot ellipsis">${doc.next_due_date}</span>`;
+        }
+    }
+};
